7ce50abb98f2e0c7cf4288794bcb977af6bc4074
root <root@DESKTOP-276TNP6>
2023-02-10 16:31:59 +0100

    comment these two files

--- src/main/java/org/apache/flink/metrics/csv/CsvMetricReporter.java
+++ src/main/java/org/apache/flink/metrics/csv/CsvMetricReporter.java
@@ -1,55 +1,55 @@
-package org.apache.flink.metrics.csv;
+// package org.apache.flink.metrics.csv;
 
-import org.apache.flink.metrics.Metric;
-import org.apache.flink.metrics.MetricConfig;
-import org.apache.flink.metrics.MetricGroup;
-import org.apache.flink.metrics.reporter.MetricReporter;
+// import org.apache.flink.metrics.Metric;
+// import org.apache.flink.metrics.MetricConfig;
+// import org.apache.flink.metrics.MetricGroup;
+// import org.apache.flink.metrics.reporter.MetricReporter;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Map;
+// import java.io.FileWriter;
+// import java.io.IOException;
+// import java.util.Map;
 
-public class CsvMetricReporter implements MetricReporter {
+// public class CsvMetricReporter implements MetricReporter {
 
-    private FileWriter writer;
+//     private FileWriter writer;
 
-    @Override
-    public void open(MetricConfig config) {
-        try {
-            writer = new FileWriter("metrics.csv");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+//     @Override
+//     public void open(MetricConfig config) {
+//         try {
+//             writer = new FileWriter("metrics.csv");
+//         } catch (IOException e) {
+//             e.printStackTrace();
+//         }
+//     }
 
-    @Override
-    public void close() {
-        try {
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+//     @Override
+//     public void close() {
+//         try {
+//             writer.close();
+//         } catch (IOException e) {
+//             e.printStackTrace();
+//         }
+//     }
 
-    @Override
-    public void report() {
-        for (Map.Entry<String, String> entry : group.getAllVariables().entrySet()) {
-            try {
-                writer.write(group.toString() + "," + entry.getKey() + "," + entry.getValue());
-                writer.write("\n");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+//     @Override
+//     public void report() {
+//         for (Map.Entry<String, String> entry : group.getAllVariables().entrySet()) {
+//             try {
+//                 writer.write(group.toString() + "," + entry.getKey() + "," + entry.getValue());
+//                 writer.write("\n");
+//             } catch (IOException e) {
+//                 e.printStackTrace();
+//             }
+//         }
+//     }
 
-    @Override
-    public void notifyOfAddedMetric(Metric metric, String metricName, MetricGroup group) {
-        // TODO
-    }
+//     @Override
+//     public void notifyOfAddedMetric(Metric metric, String metricName, MetricGroup group) {
+//         // TODO
+//     }
 
-    @Override
-    public void notifyOfRemovedMetric(Metric metric, String metricName, MetricGroup group) {
-        // TODO
-    }
-}
+//     @Override
+//     public void notifyOfRemovedMetric(Metric metric, String metricName, MetricGroup group) {
+//         // TODO
+//     }
+// }
